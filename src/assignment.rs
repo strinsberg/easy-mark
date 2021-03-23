a03a1e0345f52991f973c7ad24d04d9c9a2ccd5f
strinsberg <stevendkom@gmail.com>
2021-03-23 04:18:24 +0000

    Added new grade sheet helper and removed some privacy for comments as it is not necessary.

--- src/assignment.rs
+++ src/assignment.rs
@@ -3,7 +3,7 @@ use crate::comment::Comment;
 
 
 #[derive(Debug)]
-struct CommentAndCount(Comment, u32);
+pub struct CommentAndCount{ com: Comment, count: u32 }
 
 #[derive(Debug)]
 pub struct Assignment<'a> {
@@ -11,7 +11,7 @@ pub struct Assignment<'a> {
     pub course: String,
     pub marks: Vec<Vec<u32>>,
     pub grade_sheets: Vec<GradeSheet<'a>>,
-    comments: Vec<CommentAndCount>,
+    pub comments: Vec<CommentAndCount>,
 }
 
 impl<'a> Assignment<'a> {
@@ -27,31 +27,21 @@ impl<'a> Assignment<'a> {
 
     pub fn new_comment(&mut self, deduction: u32, text: String) {
         self.comments.push(
-            CommentAndCount(
-                Comment::new(deduction, text),
-                1,
+            CommentAndCount {
+                com: Comment::new(deduction, text),
+                count: 1,
+            }
+        );
+    }
+
+    pub fn new_grade_sheet(&mut self, student: String) {
+        self.grade_sheets.push(
+            GradeSheet::new(
+                student,
+                &self.marks,
             )
         )
     }
-
-    pub fn get_comments(&self) -> Vec<&Comment> {
-        self.comments
-            .iter()
-            .map(|c| &c.0)
-            .collect()
-    }
-
-    pub fn com_count_inc() {}
-
-    pub fn com_count_dec() {}
-
-    pub fn num_comments(&self) -> usize {
-        self.comments.len()
-    }
-
-    pub fn add_student() {}
-
-    pub fn remove_student() {}
 }
 
 
@@ -71,7 +61,7 @@ mod tests {
         assert_eq!(String::from("CPSC 3720"), asn.course);
         assert_eq!(2, asn.marks[0][0]);
         assert_eq!(10, asn.marks[1][0]);
-        assert_eq!(0, asn.num_comments());
+        assert_eq!(0, asn.comments.len());
     }
 
     #[test]
@@ -85,11 +75,24 @@ mod tests {
             2,
             String::from("some comment text")
         );
-        assert_eq!(1, asn.num_comments());
-        let coms = asn.get_comments();
-        assert_eq!(2, coms[0].deduction);
+        assert_eq!(1, asn.comments.len());
+        assert_eq!(1, asn.comments[0].count);
+        assert_eq!(2, asn.comments[0].com.deduction);
+        assert_eq!("some comment text", asn.comments[0].com.text);
     }
 
+    #[test]
+    fn it_creates_a_() {
+        let mut asn = Assignment::new(
+            String::from("Assignment 1"),
+            String::from("CPSC 3720"),
+            vec![ vec![2, 4], vec![10]],
+        );
+        asn.new_grade_sheet(String::from("Steven"));
+        assert_eq!(1, asn.grade_sheets.len());
+        assert_eq!(String::from("Steven"), asn.grade_sheets[0].student);
+        assert_eq!(16, asn.grade_sheets[0].total);
+    }
 
 }
 
